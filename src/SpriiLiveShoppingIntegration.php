aa54f565168c27152d6fc50adf69eacb33fe469a
Yashvin Veerappa <yashvin.veerappa@gmail.com>
2024-06-05 09:24:30 +0400

    updates

--- src/SpriiLiveShoppingIntegration.php
+++ src/SpriiLiveShoppingIntegration.php
@@ -2,21 +2,21 @@
 
 namespace Sprii\LiveShoppingIntegration;
 
-use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Context\UpdateContext;
 use Shopware\Core\System\CustomField\CustomFieldTypes;
 
-/**
- * Class LiveShoppingIntegration
- * @package Sprii\LiveShoppingIntegration
- */
 class SpriiLiveShoppingIntegration extends Plugin
 {
+    const SPRII_ORDER_FIELDS = "sprii_order_fields";
     const SPRII_CART_REFERENCE_ID = "ref_id";
+    const SPRII_CART_EXPORTED = "sprii_exported";
     const SPRII_CART_LINEITEM = "sprii_lineitem";
     const SPRII_SET_PRICE = "sprii_set_price";
 
@@ -24,14 +24,35 @@ class SpriiLiveShoppingIntegration extends Plugin
     {
         parent::install($installContext);
 
+        $this->removeExistingCustomFields($installContext->getContext());
+        $this->createCustomFieldSet($installContext);
+    }
+
+    public function update(UpdateContext $updateContext): void
+    {
+        parent::update($updateContext);
+
+        $this->removeExistingCustomFields($updateContext->getContext());
+        $this->createCustomFieldSet($updateContext);
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        parent::uninstall($uninstallContext);
+
+        $this->removeExistingCustomFields($uninstallContext->getContext());
+    }
+
+    public function createCustomFieldSet(InstallContext|UpdateContext $context): void
+    {
         $customFieldSetRepository = $this->container->get('custom_field_set.repository');
 
         $customFieldSetRepository->upsert([
             [
-                'name' => 'sprii_order_fields',
+                'name' => self::SPRII_ORDER_FIELDS,
                 'customFields' => [
                     [
-                        'name' => SpriiLiveShoppingIntegration::SPRII_CART_REFERENCE_ID,
+                        'name' => self::SPRII_CART_REFERENCE_ID,
                         'type' => CustomFieldTypes::TEXT,
                         'config' => [
                             'label' => [
@@ -41,6 +62,19 @@ class SpriiLiveShoppingIntegration extends Plugin
                             ],
                             'customFieldType' => 'text',
                             'customFieldPosition' => 1,
+                        ]
+                    ],
+                    [
+                        'name' => self::SPRII_CART_EXPORTED,
+                        'type' => CustomFieldTypes::BOOL,
+                        'config' => [
+                            'label' => [
+                                'da-DK' => 'Sprii Ordre eksporteret',
+                                'de-DE' => 'Sprii Bestellung exportiert',
+                                'en-GB' => 'Sprii Order Exported'
+                            ],
+                            'customFieldType' => 'bool',
+                            'customFieldPosition' => 2,
                         ]
                     ]
                 ],
@@ -57,27 +91,37 @@ class SpriiLiveShoppingIntegration extends Plugin
                     ]
                 ],
             ]
-        ], $installContext->getContext());
+        ], $context->getContext());
     }
 
-    public function uninstall(UninstallContext $uninstallContext): void
+    public function removeExistingCustomFields(Context $context): void
     {
-        parent::uninstall($uninstallContext);
-
-        if ($uninstallContext->keepUserData()) {
-            return;
-        }
-
         $customFieldSetRepository = $this->container->get('custom_field_set.repository');
+        $customFieldRepository = $this->container->get('custom_field.repository');
 
         $criteria = new Criteria();
         $criteria->addFilter(
-            new EqualsFilter('custom_field_set.name', SpriiLiveShoppingIntegration::SPRII_CART_REFERENCE_ID)
+            new EqualsFilter('custom_field_set.name', self::SPRII_ORDER_FIELDS)
         );
 
-        $customFields = $customFieldSetRepository->search($criteria, $uninstallContext->getContext());
+        $customFieldSet = $customFieldSetRepository->search($criteria, $context);
+
+        foreach ($customFieldSet->getEntities()->getElements() as $key => $customField) {
+            $customFieldSetRepository->delete([["id" => $key]], $context);
+        }
+
+        $criteria = new Criteria();
+        $criteria->addFilter(
+            new EqualsAnyFilter(
+                'custom_field.name',
+                [self::SPRII_CART_REFERENCE_ID, self::SPRII_CART_EXPORTED]
+            )
+        );
+
+        $customFields = $customFieldRepository->search($criteria, $context);
+
         foreach ($customFields->getEntities()->getElements() as $key => $customField) {
-            $customFieldSetRepository->delete([["id" => $key]], $uninstallContext->getContext());
+            $customFieldRepository->delete([["id" => $key]], $context);
         }
     }
 }
